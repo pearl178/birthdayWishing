02aab68b7ea3b3a3dfc07950fbec1c50d564b80d
pearl178 <89487585+pearl178@users.noreply.github.com>
2022-05-16 14:29:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-##################### Extra Hard Starting Project ######################
 import pandas as pd
 import datetime as dt
 import random
@@ -6,9 +5,7 @@ import smtplib
 
 my_email = "charbat.serikbol@gmail.com"
 password = '117878Tian'
-# 1. Update the birthdays.csv
 
-# 2. Check if today matches a birthday in the birthdays.csv
 now = dt.datetime.now()
 month_now = now.month
 day_now = now.day
@@ -19,13 +16,12 @@ for index, row in df.iterrows():
         receive_name = row['name']
         receive_email = row['email']
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-letter_num = random.randint(1,3)
+
+letter_num = random.randint(1,2)
 with open(f"letter_templates/letter_{letter_num}.txt") as letter_file:
     sample_letter = letter_file.read()
 letter_to_send = sample_letter.replace('[NAME]', receive_name)
-letter_to_send = letter_to_send.replace('Angela', 'Pearl')
-# 4. Send the letter generated in step 3 to that person's email address.
+
 with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
     connection.starttls()
     connection.login(user=my_email, password=password)
